35674f525a07ee619cce2fd61eca7eddb9010216
712Kunal <jadhavkun0@gmail.com>
2024-10-13 23:29:23 +0530

    added the redux toolkit in the package.json

--- src/components/UI/LandingPage.jsx
+++ src/components/UI/LandingPage.jsx
@@ -10,64 +10,23 @@ function LandingPage() {
         </h1>
       </div>
 
-      <p className="text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-        consectetur optio asperiores, error quos iusto consequatur nulla
-        nesciunt iste suscipit impedit placeat totam quibusdam omnis dolores
-        fugit reiciendis veniam, a in et ipsum. Sequi quas, nihil in, earum
-        cumque error dolorem fuga minus molestiae ad corporis ut recusandae odio
-        tempore hic nam, quod beatae. Ad aperiam nemo quibusdam praesentium
-        nihil, perferendis blanditiis quidem facilis dolores accusantium
-        incidunt unde et exercitationem, debitis a ab in itaque ea voluptates!
-        Sequi cum enim fugit cupiditate repellat commodi quos. Possimus
-        consequatur libero, porro commodi illo harum exercitationem fugit
-        accusantium dicta eum mollitia id quo aliquid quasi illum ipsa?
-        Expedita, quaerat. Autem, obcaecati doloremque quam veniam placeat
-        adipisci vero hic consequuntur voluptatum sequi perspiciatis perferendis
-        reiciendis doloribus eius iste corrupti ratione tempore suscipit
-        officia? Molestias voluptatibus placeat nisi rem, officia tempora
-        obcaecati, eveniet hic magni animi ullam reiciendis pariatur excepturi
-        repellendus culpa sequi, esse laudantium provident. Recusandae, qui
-        aliquid. Eaque necessitatibus ex iste veniam obcaecati et, velit maiores
-        odio rem non recusandae officiis. Sapiente quod reiciendis eaque
-        consequuntur, minus amet debitis quibusdam obcaecati quis fuga aliquam?
-        Modi, aliquid. Provident, suscipit quaerat temporibus dolor consequatur
-        dicta cum similique dolore veritatis id deserunt cumque error deleniti
-        consequuntur ut aliquid nam unde ab iure eveniet doloremque amet dolorum
-        quo? Similique, architecto? Omnis qui eveniet tempore assumenda minima
-        consequatur excepturi, voluptates sit, pariatur animi, quo fugiat quae
-        a. Alias expedita ullam suscipit quod, consequatur perspiciatis rem
-        veniam saepe ut quaerat pariatur delectus voluptas quasi dolorem
-        temporibus nihil at mollitia corrupti, voluptates voluptatibus enim
-        accusantium, aut dolorum. Iure ex officia recusandae, assumenda
-        reiciendis asperiores? Quaerat sunt deserunt deleniti fugit nihil,
-        recusandae voluptatibus voluptates ipsum ab ex ducimus quia iste nemo
-        mollitia modi porro repudiandae laborum laboriosam vitae ratione!
-        Aliquam quis quam reprehenderit. Et, provident labore. Consectetur
-        consequuntur placeat, quis dicta, nisi praesentium cum fugiat
-        accusantium aut dolor cumque perspiciatis blanditiis at totam, adipisci
-        voluptate repellendus! Reiciendis, nemo! Accusamus facilis fugiat vero
-        ut, nisi alias eum expedita deleniti, excepturi nam esse aperiam. Esse
-        atque tempora porro doloribus accusamus, illo obcaecati iste iusto saepe
-        sapiente mollitia dolorem aliquam corporis unde facere. Adipisci commodi
-        sit iure nulla vitae quos, vero debitis, nesciunt illum vel unde
-        sapiente eius, distinctio iusto! Nobis accusantium placeat provident
-        molestias ad velit dolorum, earum, sint dignissimos dolor quas itaque,
-        eius cupiditate vitae dolores. Quod corrupti, ut sit nihil dolorum,
-        nulla aliquam non, recusandae maiores tempora officiis excepturi?
-        Consectetur natus odio quis distinctio unde labore quaerat ad pariatur,
-        minima quas tempora quia incidunt velit doloremque, nisi molestiae
-        reiciendis beatae deleniti quo delectus voluptatibus. Cupiditate quod
-        culpa necessitatibus neque aperiam commodi repellat fuga adipisci eos
-        laboriosam, illum consequuntur nihil perferendis id expedita molestias,
-        quos sequi consectetur. Doloribus minima quos maxime enim beatae, facere
-        molestiae nesciunt assumenda culpa asperiores reiciendis alias. Culpa
-        enim, facilis adipisci temporibus nam quisquam fuga! Asperiores cumque
-        eius illo molestias autem vitae sapiente ab ipsam dolores unde enim
-        expedita temporibus impedit earum ipsum quisquam velit nostrum esse a
-        nesciunt vel obcaecati perspiciatis, consequuntur officiis. Animi
-        accusamus quasi voluptate.
-      </p>
+      <div>
+        <div className="bg-[url('./assets/svgs/landingPageWave1.svg')] bg-cover bg-center h-screen w-full flex grow">
+          <div className="flex grow flex-col w-96 items-center justify-center border-2 border-cyan-400 relative top-44 left-1/2 inset-y-0 text-white">
+            <h1>AI-Powered Planning</h1>
+            <p>
+              Our advanced AI analyzes habits and travel trends, booking and
+              budget management
+            </p>
+            <div className="w-full flex items-center my-2">
+              <div className="flex-grow h-px bg-gray-600"></div>
+              <div className="flex-grow h-px bg-gray-600"></div>
+            </div>
+            <span>Data-driven insights</span>
+            <span>Dynamic adjustments</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
